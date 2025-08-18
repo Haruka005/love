68daf3bb06097de6f2ac3575ed40ee70eeaac978
Haruka005 <HC431008@g.neec.ac.jp>
2025-08-18 14:51:42 +0900

    合体準備

--- React_project/src/App.js
+++ React_project/src/App.js
@@ -1,50 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import logo from './anpan-man-.png';
-import './App.css';
-import Header from './Header';
+import React from 'react';  //reactを使うための宣言
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';  //画面遷移をする道具を使う宣言
+import Login from './Login';  //遷移先をインポート
+import Signup from './Signup';
+import LoginComplete from './LoginComplete';
+import AccountCreated from './AccountCreated';
 
-function App() {
-  const [position, setPosition] = useState({ top: 100, left: 100 });
+function App() {  //App関数を作る
+  return (  //画面の表示内容を以下に記述
+    <BrowserRouter>   {/*全体を遷移できる道具で管理する*/}
+      <Routes>  {/*URLに応じてどのページを表示するかを決めるブロック*/}
+        {/* トップページ */}
+        <Route path="/" element={   //'/'は最初に開くURLのことelementはその中身のこと
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newTop = Math.floor(Math.random() * (window.innerHeight - 150)); // 高さの範囲内
-      const newLeft = Math.floor(Math.random() * (window.innerWidth - 150)); // 幅の範囲内
-      setPosition({ top: newTop, left: newLeft });
-    }, 1000); // 0.5秒ごとに位置変更
+          <div>   {/*パスが最初のURLだったら以下を表示するよ！*/}
+            <h1>トップページ</h1>
+            
+            <Link to="/login">
+              <button>ログインページへ</button>
+            </Link>
 
-    return () => clearInterval(interval); // クリーンアップ
-  }, []);
+            <Link to = "/Signup">
+              <button>新規会員登録</button>
+            </Link>
 
-  return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <a
-         href= "https://www.anpanman.jp/about/friends/4zvqlpab9ixejb6d.html"
-         target="_blank"
-         rel="noopener noreferrer"
-        >
-         <img
-          src={logo}
-          alt="anpan"
-          className="Anpan"
-          
-          style={{
-            position: 'absolute',
-            top: `${position.top}px`,
-            left: `${position.left}px`,
-            transition: 'all 0.8s ease-in-out',
-            zIndex: 10, // 数字が大きいほど手前に表示されてる
-            cursor: 'pointer', // 視覚的にクリックできるように
-            pointerEvents: 'auto'
-          }}
+          </div>
 
-         />
-        </a>
-        <p>アンパンマンをクリックして！</p>
-      </header>
-    </div>
+        } />
+
+        {/* ログインページ */}
+        <Route path="/login" element={<Login />} />   {/*"/Login"にアクセスしたらLoginページを表示するよ！*/}
+
+        {/* ログイン完了ページ */}
+        <Route path = "/LoginComplete" element = {<LoginComplete/>} />
+
+        {/* 新規会員登録 */}
+        <Route path = "/Signup" element = {<Signup/>} />
+
+        {/* 会員登録完了ページ */}
+        <Route path = "/AccountCreated" element = {<AccountCreated/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ React_project/src/Login.js
@@ -0,0 +1,45 @@
+import { useState } from "react";   //Reactの中からuseStateっていう便利機能を取りだして使うよっていう宣言
+import { Navigate, useNavigate } from "react-router-dom";
+
+export default function Login(){    //外に持って行ってOKなLoginっていう名前の部品作るよっていう宣言
+
+    const [email,setEmail] = useState('');  //emailっていう変数用意して最初は空にしとく、メールの内容をリアルタイムで保存できる
+    const [password,setpassword] = useState('');
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {   //handle＝操作、submit＝送信つまり送信された時にやること(e)はイベント情報が入っている（いつどのキーがクリックされた？）=>とはこれ(送信操作)がされたら次の処理を実行して！ということ
+        e.preventDefault(); //フォーム送信時は自動でリロードされてしまい、入力内容が消えてしまう。そのためページのリロードを防ぐ関数を用いる
+
+        if(email.trim() !== "" && password.trim() !== ""){
+            navigate('/LoginComplete')
+        }else{
+            setError("メールアドレスまたはパスワードが違います");
+        }
+
+        console.log('ログイン情報：',{email,password});     //開発者用画面(F12)で表示される。無くてもいいがちゃんと動いてるか確認ができるから便利
+    };
+
+    return(     //ここからどんな見た目にするか書く
+        <form onSubmit = {handleSubmit}>    {/*formは入力フォームを作るタグでonSubmitはこのフォームが送信されたときという意味={handleSubmit}でこのフォームを送信したときにhandleSubmitを実行して！という意味 */}
+            <h2>ログイン</h2> 
+            <input 
+                type="email"    //この入力欄はメールアドレス専用と宣言(入力ミスとかも見てくれる)
+                placeholder="メールアドレス"    //入力欄に薄く表示される文字、入力したら消える
+                value={email}   //reactが持ってるemail関数の値を表示してねという意味入力した値がリアルタイムで入力欄に表示、これがないとreact側が入力内容を保持できない
+                onChange={(e) => setEmail(e.target.value)}  //入力されたらReactの状態を更新してね！の意味。e=入力されたという情報、setEmail()の中にe.taeget.value（入力された文字を取り出す）を入れることでEmail変数の中身が入力された内容に変化する
+                required    //空はNGの意味（ブラウザが自動でエラーメッセージを表示する）Submitできない
+            />
+            <input
+                type="password"
+                placeholder="パスワード"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
+                required
+            />
+            
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <button type="submit">ログイン</button> {/*フォームの送信ボタン、押したらhandleSubmitが動く*/}
+        </form>
+    );
+}
