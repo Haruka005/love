01695eacaae90852acdb61c4a875542386f47807
iida818 <HC431019@g.neec.ac.jp>
2025-10-16 15:46:18 +0900

    11

--- laravel_project/app/Http/Controllers/AuthController.php
+++ laravel_project/app/Http/Controllers/AuthController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+
 
 
 //laravelの基本コントローラーを継承
@@ -33,11 +38,22 @@ class AuthController extends Controller
             ],422);//HTTPステータスコード:422(処理できない入力)
         }
 
+           $user = User::create([
+            'name' => $request ->name,
+            'email' =>$request ->email,
+            'password' => Hash::make($request->password),
+            'user_status' =>1//利用者として登録
+        ]);
+
+
         //バリデーションが成功した場合の処理（仮）
         //DB保存はまだしないが、受け取ったnameとemailを返す
         return response() ->json([
             'message' => '登録処理は成功しました（仮）',
-            'data' =>$request->only(['name','email'])//nameとemailだけを抜け出して返す
+            'data' =>[
+                'name'=>$user->name,
+                'email'=>$user->email
+            ]  
         ],200);//HTTPステータスコード:200(成功)
     }
 }
