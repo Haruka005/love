05eabc539fb9f330e38ff291de6a1150d959240f
Haruka005 <HC431008@g.neec.ac.jp>
2025-11-12 11:23:15 +0900

    a

--- laravel_project/routes/api.php
+++ laravel_project/routes/api.php
@@ -34,34 +34,5 @@ Route::post('/register', [UserController::class, 'register']);
 //ログイン
 Route::post('/login', [UserController::class, 'login']);
 
-//管理者ユーザー取得
-Route::get('/users', [AdminController::class, 'user_all']);
-
-Route::get('/geocode', function (Request $request) {
-    $address = $request->query('q');
-    if (!$address || strlen($address) < 3) {
-        return response()->json(['error' => '住所が不正です'], 400);
-    }
-    $response = Http::withOptions(['verify' => false])->get('https://nominatim.openstreetmap.org/search', [
-        'format' => 'json',
-        'q' => $address,
-    ]);
-    return $response->json();
-});
 
 
-Route::get('/events/{year}/{month}', [EventController::class, 'getByMonth']);
-Route::get('/events/upcoming', [EventController::class, 'getUpComingEvent']);
-Route::post('/store-event-data', [EventImageController::class, 'storeEventData']);
-
-// 認証必須ルート
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', fn(Request $request) => response()->json($request->user()));
-    Route::post('/upload-event-image', [EventImageController::class, 'uploadEventImage']);
-    Route::post('/store-restaurant-data', [RestaurantController::class, 'storeRestaurantData']);
-});
-
-// バージョン付きルート（例：v1）
-Route::prefix('v1')->group(function () {
-    Route::post('/store-event-detail', [EventDetailController::class, 'store']);
-});
