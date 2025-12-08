fd5bb9ff32d19ee96860820f3525fc1ad212035a
nkhs-hc43 <hc431019@g.neec.ac.jp>
2025-12-08 15:41:19 +0900

    12

--- laravel_project/app/Http/Middleware/CorsMiddleware.php
+++ laravel_project/app/Http/Middleware/CorsMiddleware.php
@@ -20,7 +20,7 @@ class CorsMiddleware
         $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+       // $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         return $response;
     }
