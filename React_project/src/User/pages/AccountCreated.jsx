43d58de66bd6446c3cc1846275163f87be171f7c
hc431008 <hc431008@g.neec.ac.jp>
2026-01-30 12:01:28 +0900

    ちょっと変えた

--- React_project/src/User/pages/AccountCreated.jsx
+++ React_project/src/User/pages/AccountCreated.jsx
@@ -1,21 +1,23 @@
-//新規登録画面
-import { useLocation,Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
-
-
-export default function AccountCreated(){
+export default function AccountCreated() {
     const location = useLocation();
-    const { name,email }=location.state || {};
+    const { name, email } = location.state || {};
 
-    
-    return(
-        <div>
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h2 style={{ color: '#F93D5D' }}>Loveりべつ</h2>
+            
             <h1>仮会員登録が完了しました</h1>
-           <p>登録されたメールアドレスに認証リンクが送信されました。</p>
-           <p>60分以内に認証を完了してください。</p>
-            <Link to = "/">
-                <button>TOPページに戻る</button>
-            </Link>
+            
+            <p>登録されたメールアドレスに認証リンクが送信されました。</p>
+            <p>60分以内に認証を完了してください。</p>
+
+            <div style={{ marginTop: '20px' }}>
+                <Link to="/">
+                    <button>TOPページに戻る</button>
+                </Link>
+            </div>
         </div>
     );
 }
